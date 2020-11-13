9099d9346ffd0f84428d35eebb6db0e06659e1a6
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-11-13 19:46:30 +0700

    Login page added

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,79 @@
 /*
  * @Author: mkamadeus
  * @Date: 2020-11-13 14:02:14
- * @Last Modified by:   mkamadeus
- * @Last Modified time: 2020-11-13 14:02:14
+ * @Last Modified by: mkamadeus
+ * @Last Modified time: 2020-11-13 19:46:15
  */
 
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 
+type LoginForm = {
+  username: string;
+  password: string;
+};
 const LoginPage = () => {
-  return <div>Hello</div>;
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const history = useHistory();
+
+  const onSubmit = () => {
+    // TODO : Verify
+
+    // FIXME : If verified, navigate
+    if (true) {
+      history.push("/");
+    }
+  };
+
+  return (
+    <div className="bg-gradient-to-r from-teal-400 to-blue-500">
+      <div className="container mx-auto flex justify-center items-center h-screen">
+        <div className="flex flex-col rounded shadow p-2 bg-white">
+          <div className="mb-2">
+            <h1 className="text-3xl font-bold text-center">Login</h1>
+          </div>
+          <div className="p-2">
+            <form className="flex-flex-col" onSubmit={handleSubmit(onSubmit)}>
+              <div className="mt-2">
+                <label htmlFor="username">Username</label>
+                <input
+                  className="rounded border border-gray-300 w-full p-1 focus:outline-none"
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  ref={register}
+                />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="password">Password</label>
+                <input
+                  className="rounded border border-gray-300 w-full p-1 focus:outline-none"
+                  type="password"
+                  name="password"
+                  placeholder="*******"
+                  ref={register}
+                />
+              </div>
+              <div className="mt-4">
+                <button
+                  type="submit"
+                  className="w-full bg-blue-600 hover:bg-blue-700 text-white rounded shadow hover:shadow-lg p-2 focus:outline-none transition duration-150"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+            {/* {errors && (
+              <div className="bg-red-300 border border-red-400 rounded text-red-700 p-1">
+                Invalid Credentials
+              </div>
+            )} */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
